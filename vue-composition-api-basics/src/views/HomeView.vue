f811fb4c386fe90fdb4723d0b75acba9b554d8cc
PwS <pawanasutha02@gmail.com>
2024-05-01 22:13:13 +0700

    Section 5

--- vue-composition-api-basics/src/views/HomeView.vue
+++ vue-composition-api-basics/src/views/HomeView.vue
@@ -4,10 +4,15 @@
 <h2>{{ appTitle }}</h2>
   <h3>{{ counterData.title }} : </h3>
     <div>
-      <button @click="decreaseCounter" class="btn">-</button>
+      <button @click="decreaseCounter(2)" class="btn">--</button>
+      <button @click="decreaseCounter(1)" class="btn">-</button>
       <span class="counter">{{ counterData.count }}</span>
-      <button @click="increaseCounter" class="btn">+</button>
+      <button @click="increaseCounter(1, $event)" class="btn">+</button>
+      <button @click="increaseCounter(2)" class="btn">++</button>
     </div>
+
+    <p>
+    This Counter is {{oddOrEven}} </p>
 
     <div class="edit">
       <h4>Edot Counter Title:</h4>
@@ -19,28 +24,48 @@
 
 <!-- composition API - script setup pattern -->
 <script setup>
-import { ref,reactive } from 'vue'
+import { ref,reactive,computed,watch } from 'vue'
 
 const appTitle='My Counter App'
 
 // const counter = ref(0),counterTitle = ref('Counter')
 
-
 // reactive object
 const counterData=reactive({
   count:0,
   title:"My Counter"
-
 })
 
-function decreaseCounter() {
+// computed property
+const oddOrEven = computed(() => {
+  if(counterData.count %2 ===0){
+    return 'even'
+  }
+  return 'odd'
+})
+
+//watch
+watch(()=>counterData.count,(newValue,oldValue)=>{
+  if(newValue===20){
+    alert('Way to go! you made it to 20!!')
+  }
+  console.log("newCount "+newValue,oldValue)
+})
+
+
+function decreaseCounter(value) {
   // counter.value--
-  counterData.count--
+  counterData.count -=value
 }
-function increaseCounter() {
+function increaseCounter(value,event) {
   // counter.value++
-  counterData.count++
+  counterData.count +=value
 }
+
+// const increaseCounter =(value,e)=>{
+//   console.log(event)
+//   counterData.count +=value
+// }
 
 </script>
 
@@ -83,6 +108,29 @@ export default{
  }
 }
 </script> -->
+
+// <script>
+// export default {
+//   data() {
+//     return {
+//       count:0
+//     }
+//   },
+//   computed:{
+//     myComputedProperty(){
+//       // return some logic based on data property
+//       return 'my result'
+//     }
+//   },
+//   watch: {
+//     count(newCount,oldCount){
+//       if(newCount==20){
+//         alert('20 Value')
+//       }
+//     }
+//   },
+// }
+// </script>
 
 <style>
 .home {
